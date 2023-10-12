7f1df67b9aedab1438a36b53243effbbd38d4ec3
ring0162 <arringham@gmail.com>
2023-10-12 14:23:52 +0100

    Working! Takes planet radius from secondary

--- starry/kepler.py
+++ starry/kepler.py
@@ -1043,11 +1043,12 @@ class System(object):
             xpos, ypos, zpos = self.position(t)
             xpos = xpos[1]
             ypos = ypos[1]
-            theta = np.linspace(0, 2 * np.pi, int(self.primary.map.nt))
             for body in self.secondaries:
                 #assign the radius of each body to a variable
-                r = body._r * np.ones_like(t)
-            return DopplerMap.flux(self.primary.map, theta = theta, method = "conv", normalize=False, xo = xpos, yo = ypos, ro = r)
+                ro = body._r * np.ones_like(t)
+                print(ro)
+                #ro = 0.2 * np.ones_like(t)
+            return DopplerMap.flux(self.primary.map, method = "conv", normalize=False, xo = xpos, yo = ypos, ro = ro)
         else:
             X = self.design_matrix(t)
             # Weight the ylms by amplitude
